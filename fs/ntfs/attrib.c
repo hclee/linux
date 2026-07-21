44f359cc1426365f795fbcdd2e45a53709803cd5
Hyunchul Lee <hyc.lee@gmail.com>
2026-07-21 14:27:58 +0900

    ntfs: lock attr_list_persist_lock in ntfs_attr_update_mapping_pairs
    Signed-off-by: Hyunchul Lee <hyc.lee@gmail.com>

--- fs/ntfs/attrib.c
+++ fs/ntfs/attrib.c
@@ -3924,7 +3924,7 @@ int ntfs_attr_update_mapping_pairs(struct ntfs_inode *ni, s64 from_vcn)
 	struct attr_record *a;
 	s64 stop_vcn;
 	int err = 0, mp_size, cur_max_mp_size, exp_max_mp_size;
-	bool finished_build, attrlist_changed = false;
+	bool finished_build, attrlist_changed = false, attrlist_locked = false;
 	bool first_updated = false;
 	struct super_block *sb;
 	struct runlist_element *start_rl;
@@ -4022,9 +4022,23 @@ retry:
 			continue;
 		}
 
+		/*
+		 * Serialize every possible ALE update in this iteration with
+		 * the subsequent attribute-list persistence.
+		 */
+		if (NInoAttrList(base_ni) &&
+		    ni->type != AT_ATTRIBUTE_LIST && !attrlist_locked) {
+			mutex_lock(&base_ni->attr_list_persist_lock);
+			attrlist_locked = true;
+		}
+
 		err = ntfs_attr_update_meta(a, ni, m, ctx);
 		if (err < 0) {
 			if (err == -EAGAIN) {
+				if (attrlist_locked) {
+					mutex_unlock(&base_ni->attr_list_persist_lock);
+					attrlist_locked = false;
+				}
 				ntfs_attr_put_search_ctx(ctx);
 				goto retry;
 			}
@@ -4192,9 +4206,13 @@ retry:
 
 	if (attrlist_changed) {
 		err = ntfs_attrlist_update(base_ni);
-		if (err)
-			goto put_err_out;
 	}
+	if (attrlist_locked) {
+		mutex_unlock(&base_ni->attr_list_persist_lock);
+		attrlist_locked = false;
+	}
+	if (attrlist_changed && err)
+		goto put_err_out;
 
 	/* Deallocate not used attribute extents and return with success. */
 	if (finished_build) {
@@ -4312,6 +4330,8 @@ out:
 	return 0;
 
 put_err_out:
+	if (attrlist_locked)
+		mutex_unlock(&base_ni->attr_list_persist_lock);
 	if (ctx)
 		ntfs_attr_put_search_ctx(ctx);
 	return err;
